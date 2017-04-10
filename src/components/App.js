29b1cc5cdcdb13c8a30480a9f606564afec14d64
Brian Bycynski <high5bri@hotmail.com>
2017-04-10 06:44:43 -0400

    practice on css-grid

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
+    return <div className='wrapper'>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
     </div>
   }
 }
