c7618e9285f1d2222754727142cdf9087688fc2d
Brian Bycynski <high5bri@hotmail.com>
2017-04-11 06:51:19 -0400

    added beagle pics

--- src/components/Rrouter.js
+++ src/components/Rrouter.js
@@ -1,40 +1,41 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link }
 from 'react-router-dom'
+import { Image } from 'react-bootstrap'
 const Example1 = () => (
   <div>
-    <h1>Bdog</h1>
+    <Image src='http://images3.wikia.nocookie.net/__cb20091227085815/dogs-cats/images/0/0f/Beagle-puppy.jpg' responsive />
   </div>
 )
 
 const Example2 = () => (
   <div>
-    <h1>Bdog1</h1>
+    <Image src='http://youbeagle.com/it/wp-content/uploads/2009/02/you-beagle.jpg' responsive />
   </div>
 )
 
 const Example3 = () => (
   <div>
-    <h1>Bdog2</h1>
+    <Image src='http://www.beaglepuppybreeders.co.uk/wp-content/uploads/2015/06/K-litter2.jpg' responsive />
   </div>
 )
 
 const Example4 = () => (
   <div>
-    <h1>Bdog3</h1>
+    <Image src='https://s-media-cache-ak0.pinimg.com/236x/4a/75/76/4a75764e042ca019d7d9c8ecc09b052a.jpg' responsive />
   </div>
 )
 
 export default class Rrouter extends Component {
   render () {
     return <div>
-      <h2>Examples</h2>
+      <h2>Beagle pictures</h2>
       <Router>
         <ul>
-          <li><Link to='/'>Example1</Link></li>
-          <li><Link to='/ex1'>Example2</Link></li>
-          <li><Link to='/ex2'>Example3</Link></li>
-          <li><Link to='/ex3'>Example4</Link></li>
+          <li><Link to='/'>Beage pic 1</Link></li>
+          <li><Link to='/ex1'>Beage pic 2</Link></li>
+          <li><Link to='/ex2'>Beage pic 3</Link></li>
+          <li><Link to='/ex3'>Beage pic 4</Link></li>
           <hr />
           <Route exact path='/' component={Example1} />
           <Route path='/ex1' component={Example2} />
